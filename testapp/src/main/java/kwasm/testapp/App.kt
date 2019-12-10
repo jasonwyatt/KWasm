77753ba18dff68e4f534af16926e080cac8f07c6
Jason Feinstein <jwf@google.com>
2019-12-09 19:50:54 -0800

    Fix build for testapp.

---
+++ testapp/src/main/java/kwasm/testapp/App.kt
@@ -0,0 +1,7 @@
+package kwasm.testapp
+
+import android.app.Application
+
+class App : Application() {
+    // TODO: make an app which loads a WASM .wast/.wasm file from Assets and runs it with KWasm.
+}
