12ee74880016a31e6265d00c0c4150dc58232b61
Nick Welna <nwelna@google.com>
2019-12-13 09:22:40 -0800

    Removes parens from parser

--- library/src/main/java/kwasm/format/text/MemoryType.kt
+++ library/src/main/java/kwasm/format/text/MemoryType.kt
@@ -17,7 +17,6 @@ package kwasm.format.text
 import kwasm.ast.Memory
 import kwasm.format.ParseException
 import kwasm.format.text.token.Keyword
-import kwasm.format.text.token.Paren
 import kwasm.format.text.token.Token
 
 /**
@@ -28,19 +27,13 @@ import kwasm.format.text.token.Token
  */
 @UseExperimental(ExperimentalUnsignedTypes::class)
 fun List<Token>.parseMemoryType(startingIndex: Int): ParseResult<Memory> {
-    this[startingIndex] as? Paren.Open ?: throw ParseException("Expected open paren", this[startingIndex].context)
-    val keywordIndex = startingIndex + 1
     val keyword =
-        this[keywordIndex] as? Keyword ?: throw ParseException("Expected keyword", this[keywordIndex].context)
+        this[startingIndex] as? Keyword ?: throw ParseException("Expected keyword", this[startingIndex].context)
     if (keyword.value != "memory") throw ParseException(
         "Expected 'memory' keyword but found ${keyword.value}",
         keyword.context
     )
-    val limitsIndex = startingIndex + 2
+    val limitsIndex = startingIndex + 1
     val limits = this.parseLimits(limitsIndex)
-    this[limitsIndex + limits.parseLength] as? Paren.Closed ?: throw ParseException(
-        "Expected Close Paren",
-        this[limitsIndex + limits.parseLength].context
-    )
-    return ParseResult(Memory(limits.astNode), limits.parseLength + 3)
+    return ParseResult(Memory(limits.astNode), limits.parseLength + 1)
 }
