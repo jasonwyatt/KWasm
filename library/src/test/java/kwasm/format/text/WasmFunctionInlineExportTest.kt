f03737a5ccdf810eb1339227abc4cc291bfefb34
Jason Feinstein <jwf@google.com>
2019-12-29 19:53:37 -0500

    Add inline import/export descriptors for function declarations. (#78)

---
+++ library/src/test/java/kwasm/format/text/WasmFunctionInlineExportTest.kt
@@ -0,0 +1,241 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kwasm.format.text
+
+import com.google.common.truth.Truth
+import com.google.common.truth.Truth.assertThat
+import kwasm.ast.Export
+import kwasm.ast.ExportDescriptor
+import kwasm.ast.Identifier
+import kwasm.ast.Import
+import kwasm.ast.ImportDescriptor
+import kwasm.ast.Index
+import kwasm.ast.Local
+import kwasm.ast.NumericInstruction
+import kwasm.ast.Param
+import kwasm.ast.TypeUse
+import kwasm.ast.ValueType
+import kwasm.ast.WasmFunction
+import kwasm.ast.astNodeListOf
+import kwasm.format.ParseContext
+import kwasm.format.ParseException
+import org.assertj.core.api.Assertions.fail
+import org.junit.Assert.assertThrows
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class WasmFunctionInlineExportTest {
+    private val tokenizer = Tokenizer()
+    private val context = ParseContext("WasmFunctionInlineExportTest.wat")
+
+    @Test
+    fun parse_returnsNullIf_openingParenNotFound() {
+        val result = tokenizer.tokenize("func $0 (export \"a\"))", context)
+            .parseInlineWasmFunctionExport(0)
+        assertThat(result).isNull()
+    }
+
+    @Test
+    fun parse_returnsNullIf_funcKeywordNotFound() {
+        val result = tokenizer.tokenize("(nonfunc $0 (export \"a\"))", context)
+            .parseInlineWasmFunctionExport(0)
+        assertThat(result).isNull()
+    }
+
+    @Test
+    fun parse_returnsNullIf_exportOpeningParenNotFound() {
+        val result = tokenizer.tokenize("(func $0 export \"a\"))", context)
+            .parseInlineWasmFunctionExport(0)
+        assertThat(result).isNull()
+    }
+
+    @Test
+    fun parse_returnsNullIf_exportKeywordNotFound() {
+        val result = tokenizer.tokenize("(func $0 (nonexport \"a\"))", context)
+            .parseInlineWasmFunctionExport(0)
+        assertThat(result).isNull()
+    }
+
+    @Test
+    fun parse_throwsIf_exportNameNotFound() {
+        assertThrows(ParseException::class.java) {
+            tokenizer.tokenize("(func $0 (export))", context)
+                .parseInlineWasmFunctionExport(0)
+        }
+    }
+
+    @Test
+    fun parse_throwsIf_exportClosureNotFound() {
+        val e = assertThrows(ParseException::class.java) {
+            tokenizer.tokenize("(func $0 (export \"a\"", context)
+                .parseInlineWasmFunctionExport(0)
+        }
+        assertThat(e).hasMessageThat().contains("Expected ')'")
+    }
+
+    @Test
+    fun parse_minimal() {
+        val result = tokenizer.tokenize("(func (export \"a\"))", context)
+            .parseInlineWasmFunctionExport(0)
+            ?: fail("Expected a result")
+        assertThat(result.parseLength).isEqualTo(7)
+        assertThat(result.astNode).containsExactly(
+            Export(
+                "a",
+                ExportDescriptor.Function(
+                    Index.ByIdentifier(Identifier.Function(null, null))
+                )
+            )
+        ).inOrder()
+    }
+
+    @Test
+    fun parse_simple() {
+        val result = tokenizer.tokenize("(func $0 (export \"a\"))", context)
+            .parseInlineWasmFunctionExport(0)
+            ?: fail("Expected a result")
+        assertThat(result.parseLength).isEqualTo(8)
+        assertThat(result.astNode).containsExactly(
+            Export(
+                "a",
+                ExportDescriptor.Function(
+                    Index.ByIdentifier(Identifier.Function("$0"))
+                )
+            )
+        ).inOrder()
+    }
+
+    @Test
+    fun parse_multipleExports() {
+        val result = tokenizer.tokenize(
+            "(func $0 (export \"a\") (export \"b\") (export \"c\"))",
+            context
+        ).parseInlineWasmFunctionExport(0) ?: fail("Expected a result")
+        assertThat(result.parseLength).isEqualTo(16)
+        assertThat(result.astNode).containsExactly(
+            Export(
+                "a",
+                ExportDescriptor.Function(
+                    Index.ByIdentifier(Identifier.Function("$0"))
+                )
+            ),
+            Export(
+                "b",
+                ExportDescriptor.Function(
+                    Index.ByIdentifier(Identifier.Function("$0"))
+                )
+            ),
+            Export(
+                "c",
+                ExportDescriptor.Function(
+                    Index.ByIdentifier(Identifier.Function("$0"))
+                )
+            )
+        ).inOrder()
+    }
+
+    @Test
+    fun parse_exportThenImport() {
+        val result = tokenizer.tokenize(
+            "(func $0 (export \"a\") (import \"b\" \"c\") (param i32))",
+            context
+        ).parseInlineWasmFunctionExport(0) ?: fail("Expected a result")
+        assertThat(result.parseLength).isEqualTo(17)
+        assertThat(result.astNode).containsExactly(
+            Export(
+                "a",
+                ExportDescriptor.Function(
+                    Index.ByIdentifier(Identifier.Function("$0"))
+                )
+            ),
+            Import(
+                "b",
+                "c",
+                ImportDescriptor.Function(
+                    Identifier.Function("$0"),
+                    TypeUse(
+                        null,
+                        astNodeListOf(
+                            Param(Identifier.Local(null, null), ValueType.I32)
+                        ),
+                        astNodeListOf()
+                    )
+                )
+            )
+        ).inOrder()
+    }
+
+    @Test
+    fun parse_exportThenBody() {
+        val result = tokenizer.tokenize(
+            "(func $0 (export \"a\") (local i32) (i32.add))",
+            context
+        ).parseInlineWasmFunctionExport(0) ?: fail("Expected a result")
+        assertThat(result.parseLength).isEqualTo(15)
+        assertThat(result.astNode).containsExactly(
+            Export(
+                "a",
+                ExportDescriptor.Function(
+                    Index.ByIdentifier(Identifier.Function("$0"))
+                )
+            ),
+            WasmFunction(
+                Identifier.Function("$0"),
+                TypeUse(null, astNodeListOf(), astNodeListOf()),
+                astNodeListOf(
+                    Local(null, ValueType.I32)
+                ),
+                astNodeListOf(
+                    NumericInstruction.I32Add
+                )
+            )
+        ).inOrder()
+    }
+
+    @Test
+    fun parse_multipleExportsThenBody() {
+        val result = tokenizer.tokenize(
+            "(func $0 (export \"a\") (export \"b\") (local i32) (i32.add))",
+            context
+        ).parseInlineWasmFunctionExport(0) ?: fail("Expected a result")
+        assertThat(result.parseLength).isEqualTo(19)
+        assertThat(result.astNode).containsExactly(
+            Export(
+                "a",
+                ExportDescriptor.Function(
+                    Index.ByIdentifier(Identifier.Function("$0"))
+                )
+            ),
+            Export(
+                "b",
+                ExportDescriptor.Function(
+                    Index.ByIdentifier(Identifier.Function("$0"))
+                )
+            ),
+            WasmFunction(
+                Identifier.Function("$0"),
+                TypeUse(null, astNodeListOf(), astNodeListOf()),
+                astNodeListOf(
+                    Local(null, ValueType.I32)
+                ),
+                astNodeListOf(
+                    NumericInstruction.I32Add
+                )
+            )
+        ).inOrder()
+    }
+}
