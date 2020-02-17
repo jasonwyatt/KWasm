5de2f8222b3dcb9902d988f5bee2ce4a04604e99
Jason Feinstein <jwf@google.com>
2020-02-16 22:25:47 -0800

    Create runtime Table

---
+++ library/src/main/java/kwasm/runtime/Address.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kwasm.runtime
+
+/**
+ * Addresses for [FunctionInstance]s, [Memory]s, [Table]s, and [Global]s.
+ *
+ * From [the docs](https://webassembly.github.io/spec/core/exec/runtime.html#addresses):
+ *
+ * Function instances, table instances, memory instances, and global instances in the store are
+ * referenced with abstract addresses. These are simply indices into the respective store component.
+ *
+ * ```
+ *   addr       ::= 0|1|2|...
+ *   funcaddr   ::= addr
+ *   tableaddr  ::= addr
+ *   memaddr    ::= addr
+ *   globaladdr ::= addr
+ * ```
+ *
+ * An embedder may assign identity to exported store objects corresponding to their addresses, even
+ * where this identity is not observable from within WebAssembly code itself (such as for function
+ * instances or immutable globals).
+ */
+sealed class Address(open val value: Int) {
+    data class Function(override val value: Int) : Address(value)
+    data class Table(override val value: Int) : Address(value)
+    data class Memory(override val value: Int) : Address(value)
+    data class Global(override val value: Int) : Address(value)
+}
---
+++ library/src/test/java/kwasm/runtime/TableTest.kt
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package kwasm.runtime
+
+import com.google.common.truth.Truth.assertThat
+import kwasm.ast.type.ElementType
+import kwasm.ast.type.Limits
+import kwasm.ast.type.TableType
+import org.junit.Assert.assertThrows
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class TableTest {
+    @Test
+    fun constructor_throws_ifElementsSize_greaterThanMaxSize() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Table(listOf(Address.Function(0)), maxSize = 1)
+        }.also {
+            assertThat(it).hasMessageThat()
+                .contains("Elements in table cannot exceed the maximum size: 1")
+        }
+    }
+
+    @Test
+    fun pseudoConstructor() {
+        val table = Table(TableType(Limits(0L, 10L), ElementType.FunctionReference)) {
+            it += Address.Function(1)
+        }
+        assertThat(table.elements).containsExactly(Address.Function(1))
+        assertThat(table.maxSize).isEqualTo(10)
+    }
+}
