9e3b90d6ee44576bcf4382a32349e6ba302d394a
Jason Feinstein <jwf@google.com>
2020-02-16 23:53:14 -0800

    Create ModuleInstance (#160)
    * Create ModuleInstance
    
    * ktlint

--- library/src/main/java/kwasm/runtime/ModuleInstance.kt
+++ library/src/main/java/kwasm/runtime/ModuleInstance.kt
@@ -14,4 +14,41 @@
 
 package kwasm.runtime
 
-interface ModuleInstance
+import kwasm.ast.type.FunctionType
+
+/**
+ * Represents an instantiated [kwasm.ast.module.WasmModule].
+ *
+ * From [the docs](https://webassembly.github.io/spec/core/exec/runtime.html#module-instances):
+ *
+ * A module instance is the runtime representation of a module. It is created by instantiating a
+ * module, and collects runtime representations of all entities that are imported, defined, or
+ * exported by the module.
+ *
+ * ```
+ *   moduleinst ::= {
+ *                      types functype*,
+ *                      funcaddrs funcaddr*,
+ *                      tableaddrs tableaddr*,
+ *                      memaddrs memaddr*,
+ *                      globaladdrs globaladdr*,
+ *                      exports exportinst*
+ *                  }
+ * ```
+ *
+ * Each component references runtime instances corresponding to respective declarations from the
+ * original module – whether imported or defined – in the order of their static indices. Function
+ * instances, table instances, memory instances, and global instances are referenced with an
+ * indirection through their respective addresses in the store.
+ *
+ * It is an invariant of the semantics that all export instances in a given module instance have
+ * different names.
+ */
+data class ModuleInstance(
+    val types: List<FunctionType>,
+    val functionAddresses: List<Address.Function>,
+    val tableAddresses: List<Address.Table>,
+    val memoryAddresses: List<Address.Memory>,
+    val globalAddresses: List<Address.Global>,
+    val exports: List<Export>
+)
